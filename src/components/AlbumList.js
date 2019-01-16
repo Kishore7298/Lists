75677fe46241391fea2631650ff6049e359c7575
kishore <4nm15is025@nmamit.in>
2019-01-16 17:21:33 +0530

    Refactoring Function based AlbumList to Class based

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = ()=>{
-    return (
-        <View>
-            <Text>This is album list!!</Text>
-        </View>
-    );
+class AlbumList extends React.Component {
+    render(){
+        return (
+            <View>
+                <Text>This is album list!!</Text>
+            </View>
+        )
+    }
 }
 
 export default AlbumList;
